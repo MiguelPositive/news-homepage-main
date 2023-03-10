fb1838736219ae45a5ee2b8d1c728270b33dc99e
miguel angel clavijo moreno <miguelpositive@gmail.com>
2023-03-10 17:51:15 -0500

    avance algo en el diseño responsivo

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const checkbtn = document.querySelector(".checkbtn");
 const line1 = document.getElementById("line-1");
 const line2 = document.getElementById("line-2");
+const h2 = document.querySelectorAll("h2");
 
 let temp = false;
 
@@ -11,9 +12,19 @@ const handleClick = () => {
     line1.style.cssText = "transition: 0.8s; opacity: 0";
     line2.style.cssText = "transition: 0.8s; opacity: 0";
   } else {
-    line1.style.cssText = "transition: 0.3s; opacity: 1";
-    line2.style.cssText = "transition: 0.3s; opacity: 1";
+    line1.style.cssText = "transition: 0.3s; opacity: 0.4";
+    line2.style.cssText = "transition: 0.3s; opacity: 0.4";
   }
 };
 
 checkbtn.addEventListener("click", handleClick);
+
+h2.forEach((i) => {
+  checkbtn.addEventListener("click", () => {
+    if (temp) {
+      i.style.cssText = "transition: 0.9s; opacity: 0";
+    } else if (!temp) {
+      i.style.cssText = "transition: 0.3s; opacity: 1";
+    }
+  });
+});
